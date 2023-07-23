eb3d0022e0c70ce15e1ec86b445124c85e611238
Sidhant Roymoulik <sidhant.roymoulik@gmail.com>
2023-07-22 23:09:40 -0400

    ab-pruning

--- Chess-Challenge/src/Evil Bot/EvilBot.cs
+++ Chess-Challenge/src/Evil Bot/EvilBot.cs
@@ -7,48 +7,114 @@ namespace ChessChallenge.Example
     // Plays randomly otherwise.
     public class EvilBot : IChessBot
     {
-        // Piece values: null, pawn, knight, bishop, rook, queen, king
-        int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
+        int CHECKMATE = 100000;
+
+        int time_limit = 0;
+        DateTime start = DateTime.Now;
+        bool stop_search = false;
+        Int64 nodes = 0;
 
         public Move Think(Board board, Timer timer)
         {
-            Move[] allMoves = board.GetLegalMoves();
+            time_limit = timer.MillisecondsRemaining / 120;
+            start = DateTime.Now;
+            stop_search = false;
+            nodes = 0;
 
-            // Pick a random move to play if nothing better is found
-            Random rng = new();
-            Move moveToPlay = allMoves[rng.Next(allMoves.Length)];
-            int highestValueCapture = 0;
+            Move[] moves = board.GetLegalMoves();
+            Move best_move = moves[0];
 
-            foreach (Move move in allMoves)
+            for (int depth = 1; depth < 100; depth++)
             {
-                // Always play checkmate in one
-                if (MoveIsCheckmate(board, move))
+                Move depth_move = moves[0];
+                int score = Int32.MinValue;
+                foreach (Move move in moves)
                 {
-                    moveToPlay = move;
-                    break;
+                    if (stop_search)
+                        break;
+
+                    nodes++;
+
+                    board.MakeMove(move);
+                    int new_score = -Search(board, depth - 1, 1);
+                    board.UndoMove(move);
+
+                    if (new_score > score)
+                    {
+                        score = new_score;
+                        depth_move = move;
+                    }
                 }
 
-                // Find highest value capture
-                Piece capturedPiece = board.GetPiece(move.TargetSquare);
-                int capturedPieceValue = pieceValues[(int)capturedPiece.PieceType];
+                if (stop_search)
+                    break;
 
-                if (capturedPieceValue > highestValueCapture)
+                best_move = depth_move;
+
+                if (score > CHECKMATE / 2)
+                    break;
+            }
+
+            return best_move;
+        }
+
+        public int Search(Board board, int depth, int ply)
+        {
+            if ((DateTime.Now - start).TotalMilliseconds > time_limit)
+            {
+                stop_search = true;
+                return 0;
+            }
+
+            nodes++;
+
+            Move[] moves = board.GetLegalMoves();
+
+            if (board.IsInCheckmate())
+                return -CHECKMATE + ply;
+
+            if (board.IsDraw())
+                return 0;
+
+            if (depth <= 0)
+                return Eval(board);
+
+
+            int score = Int32.MinValue;
+            foreach (Move move in moves)
+            {
+                board.MakeMove(move);
+                int new_score = -Search(board, depth - 1, ply + 1);
+                board.UndoMove(move);
+
+                if (new_score > score)
                 {
-                    moveToPlay = move;
-                    highestValueCapture = capturedPieceValue;
+                    score = new_score;
                 }
             }
 
-            return moveToPlay;
+            return score;
         }
 
-        // Test if this move gives checkmate
-        bool MoveIsCheckmate(Board board, Move move)
+        // Piece values: null, pawn, knight, bishop, rook, queen, king
+        int[] pvm_mg = { 0, 100, 320, 330, 500, 900, 20000 };
+
+        public int Eval(Board board)
         {
-            board.MakeMove(move);
-            bool isMate = board.IsInCheckmate();
-            board.UndoMove(move);
-            return isMate;
+            int turn = Convert.ToInt32(board.IsWhiteToMove);
+            int[] score = { 0, 0 };
+
+            PieceList[] all_pieces = board.GetAllPieceLists();
+
+            foreach (PieceList piece_list in all_pieces)
+            {
+                int color = Convert.ToInt32(piece_list.IsWhitePieceList);
+                PieceType type = piece_list.TypeOfPieceInList;
+
+                score[color] += pvm_mg[(int)type] * piece_list.Count;
+            }
+
+            return score[turn] - score[turn ^ 1];
         }
     }
 }
--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,54 +1,49 @@
 ﻿using ChessChallenge.API;
 using System;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
 
 public class MyBot : IChessBot
 {
     int CHECKMATE = 100000;
-
     int time_limit = 0;
     DateTime start = DateTime.Now;
-    bool stop_search = false;
+    Move depth_move = new Move();
+    Int64 nodes = 0;
 
     public Move Think(Board board, Timer timer)
     {
+        return Iterative_Deepening(board, timer);
+    }
+
+    public Move Iterative_Deepening(Board board, Timer timer)
+    {
         time_limit = timer.MillisecondsRemaining / 120;
         start = DateTime.Now;
-        stop_search = false;
-
+        nodes = 0;
 
         Move[] moves = board.GetLegalMoves();
         Move best_move = moves[0];
 
         for (int depth = 1; depth < 100; depth++)
         {
-            Move depth_move = moves[0];
-            int score = Int32.MinValue;
-            foreach (Move move in moves)
-            {
-                if (stop_search)
-                    break;
+            depth_move = moves[0];
+            int score = Search(board, depth, 0, -CHECKMATE, CHECKMATE);
 
-                board.MakeMove(move);
-                int new_score = -Search(board, depth - 1, 1);
-                board.UndoMove(move);
-
-                if (new_score > score)
-                {
-                    score = new_score;
-                    depth_move = move;
-                }
-            }
-
-            if (stop_search)
+            if ((DateTime.Now - start).TotalMilliseconds > time_limit)
                 break;
 
             best_move = depth_move;
 
-            Console.WriteLine(String.Format("depth {0} time {1} score {2} pv {3}{4}",
+            Console.WriteLine(String.Format("depth {0} score {1} nodes {2} nps {3} time {4} pv {5}{6}",
                 depth,
-                (int)(DateTime.Now - start).TotalMilliseconds,
                 score,
-                best_move.StartSquare.Name, best_move.TargetSquare.Name));
+                nodes,
+                (Int64)(1000 * nodes / ((Int64)(DateTime.Now - start).TotalMilliseconds + 1)),
+                (int)(DateTime.Now - start).TotalMilliseconds,
+                best_move.StartSquare.Name,
+                best_move.TargetSquare.Name));
 
             if (score > CHECKMATE / 2)
                 break;
@@ -58,41 +53,46 @@ public class MyBot : IChessBot
         return best_move;
     }
 
-    public int Search(Board board, int depth, int ply)
+    public int Search(Board board, int depth, int ply, int alpha, int beta)
     {
+        nodes++;
+
         if ((DateTime.Now - start).TotalMilliseconds > time_limit)
-        {
-            stop_search = true;
             return 0;
-        }
 
-        Move[] moves = board.GetLegalMoves();
+        if (board.IsInCheckmate())
+            return -CHECKMATE + ply;
 
-        if (moves.Length == 0)
-        {
-            if (board.IsInCheckmate())
-                return -CHECKMATE + ply;
+        if (board.IsDraw())
             return 0;
-        }
 
         if (depth <= 0)
             return Eval(board);
 
-
-        int score = Int32.MinValue;
+        Move[] moves = board.GetLegalMoves();
         foreach (Move move in moves)
         {
             board.MakeMove(move);
-            int new_score = -Search(board, depth - 1, ply + 1);
+            int new_score = -Search(board, depth - 1, ply + 1, -beta, -alpha);
             board.UndoMove(move);
 
-            if (new_score > score)
+            if (new_score > alpha)
             {
-                score = new_score;
+                if (ply == 0)
+                {
+                    depth_move = move;
+                }
+
+                if (new_score >= beta)
+                {
+                    alpha = beta;
+                    break;
+                }
+                alpha = new_score;
             }
         }
 
-        return score;
+        return alpha;
     }
 
     // Piece values: null, pawn, knight, bishop, rook, queen, king
